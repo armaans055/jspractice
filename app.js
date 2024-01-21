c12492dc3ec3773d74665b4e8ff67245cff7e957
Armaan Sandhu <sandhuarmaan03@icloud.com>
2024-01-21 14:26:27 -0800

    made books be added when form submit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const myLibrary = []
 currentBook = {}
 const form = document.querySelector('.adder')
+let rightSide = document.querySelector('.right-side')
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -29,9 +30,10 @@ form.addEventListener('submit', (e)=>{
     const newBook = document.createElement("div")
     newContent = document.createTextNode(currentBook.title)
     newBook.appendChild(newContent)
-    newBook.setAttribute("style","width: 30px")
-    newBook.setAttribute("style","height: 60px")
-
+    newBook.setAttribute("style","width: 5%")
+    newBook.setAttribute("style","height: 5px")
+    newBook.setAttribute("style","border: 1px solid black")
+    rightSide.appendChild(newBook)
 })
 
 
