a46909e9a1924a94b127cf03d6dda301cbb9b18c
Armaan Sandhu <sandhuarmaan03@icloud.com>
2024-01-21 14:18:44 -0800

    created classes for construcotrs and linked to form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const myLibrary = []
-
+currentBook = {}
+const form = document.querySelector('.adder')
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -17,5 +18,20 @@ function addBookToLibrary(){
         let userRead = prompt('read?')
         myLibrary.push(new Book(userTitle, userAuthor, userPages, userRead))
 }
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const fd = new FormData(form);
+    // let final = new Book(Object.fromEntries(fd))
+    // myLibrary.push(final)]
+    let currentBook = Object.fromEntries(fd)
+    myLibrary.push((Object.fromEntries(fd)))
+    const newBook = document.createElement("div")
+    newContent = document.createTextNode(currentBook.title)
+    newBook.appendChild(newContent)
+    newBook.setAttribute("style","width: 30px")
+    newBook.setAttribute("style","height: 60px")
+
+})
 
 
