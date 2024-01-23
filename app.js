79efec0ed7f97c0f307958646d846154744e892c
Armaan Sandhu <sandhuarmaan03@icloud.com>
2024-01-23 12:35:38 -0800

    finished database backend connection

--- app.js
+++ app.js
@@ -12,27 +12,40 @@ class Book {
     }
 }
 
-function addBookToLibrary(){
-        let userTitle = prompt('title?')
-        let userAuthor = prompt('author?')
-        let userPages = prompt('pages?')
-        let userRead = prompt('read?')
-        myLibrary.push(new Book(userTitle, userAuthor, userPages, userRead))
-}
+// function addBookToLibrary(){
+//         let userTitle = prompt('title?')
+//         let userAuthor = prompt('author?')
+//         let userPages = prompt('pages?')
+//         let userRead = prompt('read?')
+//         myLibrary.push(new Book(userTitle, userAuthor, userPages, userRead))
+// }
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const fd = new FormData(form);
-    // let final = new Book(Object.fromEntries(fd))
-    // myLibrary.push(final)]
+
     let currentBook = Object.fromEntries(fd)
     myLibrary.push((Object.fromEntries(fd)))
     const newBook = document.createElement("div")
-    newContent = document.createTextNode(currentBook.title)
+    newBook.className = "bookdetails"
+    newContent = document.createTextNode(currentBook.title )
+    newBook.appendChild(newContent)
+    newBook.innerHTML += "<br></br>"
+    newContent = document.createTextNode(currentBook.author)
+    newBook.appendChild(newContent)
+    newBook.innerHTML += "<br></br>"
+    newContent = document.createTextNode(currentBook.pages)
+    newBook.appendChild(newContent)
+    newBook.innerHTML += "<br></br>"
+    if(currentBook.read === "read"){
+        newContent = document.createTextNode("Read")
+    }else{
+        newContent = document.createTextNode("Unread")
+    }
     newBook.appendChild(newContent)
     newBook.setAttribute("style","width: 5%")
     newBook.setAttribute("style","height: 5px")
-    newBook.setAttribute("style","border: 1px solid black")
+    newBook.setAttribute("style","border: 5px solid black")
     rightSide.appendChild(newBook)
 })
 
